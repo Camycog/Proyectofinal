dde3890adbe5151c0c23e40380b066137a782124
Libena <li.jara@duocuc.cl>
2022-06-29 11:39:59 -0400

    cambios

--- Musica/admin.py
+++ Musica/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Genero
+from .models import Cancion
+from .models import Artista
+from .models import Album
 
 admin.site.register(Genero)
+admin.site.register(Cancion)
+admin.site.register(Artista)
+admin.site.register(Album)
