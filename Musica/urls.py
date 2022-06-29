db13556f291913714b6a752143c7fb6e2e25dc50
Camycog <cami.garcia@duocuc.cl>
2022-06-29 19:57:39 -0400

    cambios

--- Musica/urls.py
+++ Musica/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView,login
+
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path('login/', SignUpView.as_view(), name='login'),
 ]
--- Spotifly/urls.py
+++ Spotifly/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("accounts/", include("Musica.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='login.html'), name='login'),
 ]
