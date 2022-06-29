113fbec638e9960a315d20d97b19197b2c0350f4
luis <lui.medina@duocuc.cl>
2022-06-28 20:29:55 -0400

    clases

--- Musica/models.py
+++ Musica/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Genero(models.Model):
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nombre
+    
+
+class Cancion(models.Model):
+    nombre = models.CharField(max_length=100)
+    genero = models.ForeignKey(Genero, on_delete = models.DO_NOTHING)
+    artista = models.CharField(max_length=100)
+    duracion = models.IntegerField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Artista(models.Model):
+    nombre = models.CharField(max_length=100)
+    nombre_artistico = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre_artistico
+    
+class Album(models.Model):
+    nombre_album = models.CharField(max_length=100)
+        
+    def __str__(self):
+        return self.nombre_album
