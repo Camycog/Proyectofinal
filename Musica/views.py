38df56733dea89c302917745a466e980e079f1c0
Camycog <cami.garcia@duocuc.cl>
2022-06-29 15:40:21 -0400

    cambios

--- Musica/views.py
+++ Musica/views.py
@@ -8,4 +8,5 @@ from django.views import generic
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
-    template_name = "login.html"
+    template_name = "registration/signup.html"
+    
