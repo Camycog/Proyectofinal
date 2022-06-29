70c7282be9b702271ae00d548e8c77372f10068c
Camycog <cami.garcia@duocuc.cl>
2022-06-28 22:10:27 -0400

    Problemas :C

--- Spotifly/urls.py
+++ Spotifly/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Spotifly.urls')),
+    path('', include('PROBLEMA--AQUI--OJO.urls')),
 ]
