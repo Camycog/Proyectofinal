c60a1ef97a5386c88bc3f998b617fd287dfe7b43
Camycog <cami.garcia@duocuc.cl>
2022-06-29 20:17:33 -0400

    Cambios para ordenar

--- Musica/views.py
+++ Musica/views.py
@@ -10,6 +10,3 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
-    
-def login(request):
-    return render(request, 'login.html')
