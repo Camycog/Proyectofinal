c12dec8b7a4f5b246980380f88ced5cf52dcac6d
Libena <101223796+LibeKaz@users.noreply.github.com>
2022-06-29 20:39:49 -0400

    Update urls.py
    .

--- Musica/urls.py
+++ Musica/urls.py
@@ -1,9 +1,1 @@
 from django.urls import path
-from .views import SignUpView,login
-
-
-
-urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
-    path('login/', SignUpView.as_view(), name='login'),
-]
