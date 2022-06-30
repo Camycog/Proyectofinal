298761b0b8fb3e9ebda74577bd93b1f0c07669f4
Libena <101223796+LibeKaz@users.noreply.github.com>
2022-06-29 20:39:16 -0400

    Update views.py
    .

--- Musica/views.py
+++ Musica/views.py
@@ -1,8 +1,5 @@
-import re
+
 from django.shortcuts import render
 # Create your views here.
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
 
 
