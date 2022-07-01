9c1245fd2fd1a5c323d3954c5d1ade6c1e9fd63d
luis <lui.medina@duocuc.cl>
2022-07-01 19:31:11 -0400

    ok

--- Musica/models.py
+++ Musica/models.py
@@ -13,7 +13,7 @@ class Cancion(models.Model):
     nombre = models.CharField(max_length=100)
     genero = models.ForeignKey(Genero, on_delete = models.DO_NOTHING)
     artista = models.CharField(max_length=100)
-    duracion = models.IntegerField(max_length= 5)
+    duracion = models.DecimalField(max_digits=20, decimal_places=2,blank=True)
 
     def __str__(self):
         return self.nombre
