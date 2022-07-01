b4c9d6206230433865734365022c114643e7f298
Libena <li.jara@duocuc.cl>
2022-07-01 01:38:16 -0400

    arreglando

--- Spotifly/settings.py
+++ Spotifly/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from telnetlib import LOGOUT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1' , 'pythonanywhere.com']
 
-
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
--- Spotifly/urls.py
+++ Spotifly/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('Musica.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
