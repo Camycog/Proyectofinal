63b3912812fa5091c441a6328b1295e3dff15cc4
Libena <li.jara@duocuc.cl>
2022-07-01 00:00:07 -0400

    poniendo bonito el admin

--- Spotifly/settings.py
+++ Spotifly/settings.py
@@ -40,11 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Musica',
     "colorfield",
-    "django.contrib.admin",
+
 
 ]
 X_FRAME_OPTIONS = "SAMEORIGIN"
-SILENCED_SYSTEM_CHECKS = ["security.W019"]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
